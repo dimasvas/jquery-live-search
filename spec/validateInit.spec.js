c233a9d854f54a3e9a611f0aa98ef3d48fbb35ef
dimasvas <dmv.developer@gmail.com>
2016-08-25 18:57:19 +0300

    Spec style fix

--- spec/validateInit.spec.js
+++ spec/validateInit.spec.js
@@ -1,48 +1,44 @@
 describe('Live Search Unit Test', function () {
-  it('should return element withoue errors', function () {
-    var fixture = $('<input type="text" id="searchable">');
-    fixture.liveSearch({
-        url: 'database.php',
-        beforeRender: function(response){
-            return response.data;
-        },
-        data: {
-            text: 'name',
-            href: 'url' 
-        }
+    it('should return element withoue errors', function () {
+        var fixture = $('<input type="text" id="searchable">');
+        fixture.liveSearch({
+            url: 'database.php',
+            beforeRender: function(response){
+                return response.data;
+            },
+            data: {text: 'name', href: 'url'}
+        });
+        expect(fixture.length).toBe(1);
     });
-    
-    expect(fixture.length).toBe(1);
-  });
   
-  it('should throw error on empty url', function () {
-    expect(function() {
-        $.fn.liveSearch(); 
-    }).toThrow(new Error('Url is not defined!'));
-  });
+    it('should throw error on empty url', function () {
+        expect(function() {
+            $.fn.liveSearch(); 
+        }).toThrow(new Error('Url is not defined!'));
+    });
   
-  it('should throw error on beforeItemRender', function () {
-    expect(function() {
-        $.fn.liveSearch(
-            {
-                url: "fake.url"
-            }
-        ); 
-    }).toThrow(new Error('"beforeRender" Callback must be specified!'));
-  });
-  
-  it('should throw error on data attribute', function () {
-    expect(function() {
-        $.fn.liveSearch(
-            {
-                url: "fake.url",
-                beforeRender: function(data) {
-                    return data;
+    it('should throw error on beforeItemRender', function () {
+        expect(function() {
+            $.fn.liveSearch(
+                {
+                    url: "fake.url"
                 }
-            }
-        ); 
-    }).toThrow(new Error('Attributes "data.text" and "data.href" must be defined!'));
-  });
+            ); 
+        }).toThrow(new Error('"beforeRender" Callback must be specified!'));
+    });
+  
+    it('should throw error on data attribute', function () {
+        expect(function() {
+            $.fn.liveSearch(
+                {
+                    url: "fake.url",
+                    beforeRender: function(data) {
+                        return data;
+                    }
+                }
+            ); 
+        }).toThrow(new Error('Attributes "data.text" and "data.href" must be defined!'));
+    });
   
   it('should throw error on limitResult', function () {
     expect(function() {
@@ -79,5 +75,4 @@ describe('Live Search Unit Test', function () {
         ); 
     }).toThrow(new Error('Invalid "Minimum Input" option!'));
   });
-  
 });
