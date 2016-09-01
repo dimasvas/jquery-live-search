b2586b3123d8ccd33b04e94460a07ca5c4b3df96
dimasvas <dmv.developer@gmail.com>
2016-09-01 14:14:38 +0300

    Added Db faker for demo

--- demo/database.php
+++ demo/database.php
@@ -1,106 +1,68 @@
 <?php
-
+ /**
+  * Simple script that emulates Fake response to Database.
+  */   
+class DbFaker {
     
-    header('Content-Type: application/json');
-    
-    $request = $_GET['data'];
-    $data = '';
-    $reqLength = strlen($request);
-    
-    if($reqLength == 3) {
-        $data = [
-            [
-                'name'=> 'Andrey',
-                'url'=> "http://domain.com/name/Andrey"
-            ],
-            [
-                'name'=> 'Dimitry',
-                'url'=> "http://domain.com/name/Dimitry"
-            ],
-            [
-                'name'=> 'Ksu',
-                'url'=> "http://domain.com/name/Ksu"
-            ]
-        ];
-    }elseif($reqLength == 4){
-        $data = [
-            [
-                'name'=> 'John',
-                'url'=> "http://domain.com/name/John"
-            ],
-            [
-                'name'=> 'Malcovish',
-                'url'=> "http://domain.com/name/Malcovish"
-            ],
-            [
-                'name'=> 'Aria',
-                'url'=> "http://domain.com/name/Aria"
-            ],
-            [
-                'name'=> 'Domborsky',
-                'url'=> "http://domain.com/name/Domborsky"
-            ]
-        ];
-    }elseif($reqLength == 5) {
-         $data = [
-            [
-                'name'=> 'Moris',
-                'url'=> "http://domain.com/name/Moris"
-            ],
-            [
-                'name'=> 'Jake',
-                'url'=> "http://domain.com/name/Jake"
-            ],
-            [
-                'name'=> 'Mike',
-                'url'=> "http://domain.com/name/Mike"
-            ],
-            [
-                'name'=> 'Rickardo',
-                'url'=> "http://domain.com/name/Rickardo"
-            ],
-            [
-                'name'=> 'Daniel',
-                'url'=> "http://domain.com/name/Daniel"
-            ],
-            [
-                'name'=> 'McWeen',
-                'url'=> "http://domain.com/name/McWeen"
-            ],
-            [
-                'name'=> 'Stuffy',
-                'url'=> "http://domain.com/name/Stuffy"
-            ],
-            [
-                'name'=> 'Rick',
-                'url'=> "http://domain.com/name/Rick"
-            ]
-        ];
-    }elseif($reqLength == 6) {
-        $data = [
-            [
-                'name'=> 'Mikhael',
-                'url'=> "http://domain.com/name/Mikhael"
-            ],
-            [
-                'name'=> 'Rafael',
-                'url'=> "http://domain.com/name/Rafael"
-            ],
-            [
-                'name'=> 'Paul',
-                'url'=> "http://domain.com/name/Paul"
-            ],
-            [
-                'name'=> 'Sam',
-                'url'=> "Sam"
-            ]
-        ];
-    }else {
-        $data = [];
+    public function findData($request) 
+    {
+        $data = $this->getFixtures();
+        $response = [];
+        
+        foreach ($data as $value) {
+            if(stripos($value['name'], $request) !== false){
+                $response[] = $value;
+            }
+        }
+        
+        return $response;
     }
     
+    private function getFixtures() 
+    {
+        return [
+            ['name' => 'Ada',  'url'=> "http://localhost/Ada"],
+            ['name' => 'Adain', 'url'=> "http://localhost/Adain"],
+            ['name' => 'Adair', 'url'=> "http://localhost/Adair"],
+            ['name' => 'Adalbeorht', 'url'=> "http://localhost/Adalbeorht"],
+            ['name' => 'Adalbrechta', 'url'=> "http://localhost/Adalbrechta"],
+            ['name' => 'Adalson', 'url'=> "http://localhost/Adalson"],
+            ['name' => 'Adam', 'url'=> "http://localhost/Adam"],
+            ['name' => 'Adamnan', 'url'=> "http://localhost/Adamnan"],
+            ['name' => 'Adamson', 'url'=> "http://localhost/Adamson"],
+            ['name' => 'Adda', 'url'=> "http://localhost/Adda"],
+            ['name' => 'Addaneye', 'url'=> "http://localhost/Addaneye"],
+            ['name' => 'Addfwyn', 'url'=> "http://localhost/Addfwyn"],
+            ['name' => 'Addien', 'url'=> "http://localhost/Addien"],
+            ['name' => 'Addiena', 'url'=> "http://localhost/Addiena"],
+            ['name' => 'Addis', 'url'=> "http://localhost/Addis"],
+            ['name' => 'Addison', 'url'=> "http://localhost/Addison"],
+            ['name' => 'AddneyMale', 'url'=> "http://localhost/AddneyMale"],
+            ['name' => 'Addolgar', 'url'=> "http://localhost/Addolgar"],
+            ['name' => 'Addy', 'url'=> "http://localhost/Addy"],
+            ['name' => 'Addyson', 'url'=> "http://localhost/Addyson"],
+            ['name' => 'Adenydd', 'url'=> "http://localhost/Adenydd"],
+            ['name' => 'Aderyn', 'url'=> "http://localhost/Aderyn"],
+            ['name' => 'Adia', 'url'=> "http://localhost/Adia"],
+            ['name' => 'Adken', 'url'=> "http://localhost/Adken"],
+            ['name' => 'Adkins', 'url'=> "http://localhost/Adkins"],
+            ['name' => 'Adkyn', 'url'=> "http://localhost/Adkyn"],
+            ['name' => 'Adler', 'url'=> "http://localhost/Adler"],
+            ['name' => 'Adney', 'url'=> "http://localhost/Adney"],
+            ['name' => 'Adny', 'url'=> "http://localhost/Adny"],
+            ['name' => 'Adrian', 'url'=> "http://localhost/Adrian"],
+            ['name' => 'Adrion', 'url'=> "http://localhost/Adrion"],
+            ['name' => 'Adron', 'url'=> "http://localhost/Adron"],
+            ['name' => 'Adwr', 'url'=> "http://localhost/Adwr"],
+            ['name' => 'Adyna', 'url'=> "http://localhost/Adyna"]
+        ];
+    }
+}
+    
+    header('Content-Type: application/json');
+        
     echo json_encode([
         'success' => true,
         'code' => 200,
-        'data' => $data
+        'data' => (new DbFaker())->findData($_GET['data'])
     ]);
